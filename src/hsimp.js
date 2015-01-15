02458ee9248963fffb015cbc56abfdba941abcd9
Mark Wales <mark@smallhadroncollider.com>
2015-01-15 17:48:03 +0000

    moved files to src/build directories

---
+++ src/hsimp.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var main = require("main");
+var L = require("library");
+
+main.setNamedNumberDictionary(require("named-number/named-number-dictionary"));
+main.setPeriodDictionary(require("period/period-dictionary"));
+main.setCheckerDictionary(require("checker/checker-dictionary"));
+
+var hsimp = L.curry(function (options, input) {
+    if (!input || !L.isFunction(input.click)) {
+        throw new Error("input not supplied");
+    }
+
+    var classes = input.getAttribute("class") + " hsimp-level";
+    var outputText = L.isFunction(options.outputTime) ? options.outputTime : L.noOp;
+    var outputChecks = L.isFunction(options.outputChecks) ? options.outputChecks : L.noOp;
+
+    input.setAttribute("class", classes);
+
+    input.addEventListener("keyup", function () {
+        var password = main(input.value);
+
+        if (input.value.length) {
+            input.setAttribute("class", classes + " hsimp-level--" + password.getSecurityLevel());
+            outputText(password.getString(), input);
+            outputChecks(password.getChecks(), input);
+        } else {
+            input.setAttribute("class", classes);
+            outputText(false, input);
+            outputChecks([]);
+        }
+    });
+});
+
+module.exports = hsimp;
